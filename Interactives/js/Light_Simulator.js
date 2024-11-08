cfc1ae0645fe9cebf8a216983665005edd772e2c
TimothyHibbins <37994367+TimothyHibbins@users.noreply.github.com>
2024-11-08 11:59:23 +1100

    Added The Judas Goat

---
+++ Interactives/js/Light_Simulator.js
@@ -0,0 +1,21 @@
+(() => {
+  var canvas = document.createElement("canvas");
+
+  document.body.appendChild(canvas);
+
+  canvas.setAttribute("class", "interactive");
+  canvas.width = 100;
+  // canvas.height = canvas.parentElement.height;
+
+  canvas.style.background = "red";
+  // // get the context
+  // let ctx = canvas.getContext("2d");
+
+  // // set fill and stroke styles
+  // ctx.fillStyle = "#F0DB4F";
+  // ctx.strokeStyle = "red";
+
+  // // draw a rectangle with fill and stroke
+  // ctx.fillRect(50, 50, 150, 100);
+  // ctx.strokeRect(50, 50, 150, 100);
+})();
