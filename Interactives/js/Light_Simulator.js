933681d81fa99501b64af4026ce6120da4c2d5e2
TimothyHibbins <37994367+TimothyHibbins@users.noreply.github.com>
2024-11-20 17:18:08 +1100

    Update Light_Simulator.js

--- Interactives/js/Light_Simulator.js
+++ Interactives/js/Light_Simulator.js
@@ -1,21 +1,21 @@
-(() => {
-  var canvas = document.createElement("canvas");
+// (() => {
+//   var canvas = document.createElement("canvas");
 
-  document.body.appendChild(canvas);
+//   document.body.appendChild(canvas);
 
-  canvas.setAttribute("class", "interactive");
-  canvas.width = 100;
-  // canvas.height = canvas.parentElement.height;
+//   canvas.setAttribute("class", "interactive");
+//   canvas.width = 100;
+//   // canvas.height = canvas.parentElement.height;
 
-  canvas.style.background = "red";
-  // // get the context
-  // let ctx = canvas.getContext("2d");
+//   canvas.style.background = "red";
+//   // // get the context
+//   // let ctx = canvas.getContext("2d");
 
-  // // set fill and stroke styles
-  // ctx.fillStyle = "#F0DB4F";
-  // ctx.strokeStyle = "red";
+//   // // set fill and stroke styles
+//   // ctx.fillStyle = "#F0DB4F";
+//   // ctx.strokeStyle = "red";
 
-  // // draw a rectangle with fill and stroke
-  // ctx.fillRect(50, 50, 150, 100);
-  // ctx.strokeRect(50, 50, 150, 100);
-})();
+//   // // draw a rectangle with fill and stroke
+//   // ctx.fillRect(50, 50, 150, 100);
+//   // ctx.strokeRect(50, 50, 150, 100);
+// })();
