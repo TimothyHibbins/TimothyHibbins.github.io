016acbf7ff923bcddd71117e3f4e70ebbcee1dea
TimothyHibbins <37994367+TimothyHibbins@users.noreply.github.com>
2026-09-17 11:44:16 +1000

    project viewer changes

--- Interactives/js/p5_template.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-    myCanvas = document.getElementById("p5canvas")
-    createCanvas(myCanvas.offsetWidth, myCanvas.offsetHeight, myCanvas);
-}
-
-function draw() {
-    background(255);
-}
